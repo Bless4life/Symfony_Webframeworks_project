a64cacbe66a4a20688610407c00984210f64b71f
Bless4life <B00117254@mytudublin.ie>
2021-04-25 17:26:44 +0100

    added some details about suggest_idea into club fixtures, and added screenshots of the database

--- src/DataFixtures/ClubFixtures.php
+++ src/DataFixtures/ClubFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Club;
 use App\Entity\Comment;
+use App\Entity\SuggestBook;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -26,9 +27,14 @@ class ClubFixtures extends Fixture
         $club->addComment($comment1);
         $club->setName($club);
 
+        $suggest_book = new SuggestBook();
+        $suggest_book->setBookName('Hello World');
+        $suggest_book->setContext('I recommend this book');
 
+        $manager->persist($suggest_book);
         $manager->persist($comment1);
         $manager->persist($club);
+
         $manager->flush();
     }
 }
